8a70c80c28ac6752df3668bc9c6522fccc4b53b3
Sujan-Roy <sujan.007.ice@gmail.com>
2017-02-27 16:48:28 +0600

    First Commit

---
+++ app/src/main/java/com/example/android/navigationexample/FragmentModel/FragmentOne.java
@@ -0,0 +1,29 @@
+package com.example.android.navigationexample.FragmentModel;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.navigationexample.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentOne extends Fragment {
+
+
+    public FragmentOne() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fragment_one, container, false);
+    }
+
+}
